c6420f6d1d9b87c3b050e1b3504701a4701f5b50
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-10 20:03:40 +0530

    updated send email

--- APP/utils/sendEmail.py
+++ APP/utils/sendEmail.py
@@ -1,14 +1,13 @@
-from flask.app import Flask
 from flask_mail import Mail, Message
 
+# the sender should allow less secure apps for this to work
 # Sample usage: sendEmail(app, "test-subject", "test-body", ["recipient123@gmail.com"])
-def sendEmail(app: Flask, messageSubject: str, messageBody: str, recipients: list):
-	mail = Mail(app)
+def sendEmail(mail: Mail, messageSubject: str, messageBody: str, sender: str, recipients: list):
 	msg = Message(
 					messageSubject,
-					sender = app.config['MAIL_USERNAME'],
+					sender = sender,
 					recipients = recipients
 				)
 	msg.body = messageBody
 	mail.send(msg)
-	return 'Sent'
+	return 'Mail Sent'

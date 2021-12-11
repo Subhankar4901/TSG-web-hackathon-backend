efdf88c8e63d6320321cce4dda51603087268f59
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-11 09:39:49 +0530

    made send email asynchronous

--- APP/utils/sendEmail.py
+++ APP/utils/sendEmail.py
@@ -1,10 +1,19 @@
 from flask_mail import Message
-from .. import mail
+from .. import app, mail
+from threading import Thread
+
+# function which will be excuted in threads
+def send_email_thread(msg):
+	with app.app_context():
+		mail.send(msg)
 
 # the sender should allow less secure apps for this to work
-# Sample usage: sendEmail(app, "test-subject", "test-body", ["recipient123@gmail.com"])
-def sendEmail(messageSubject: str, messageBody: str,recipients: list):
-	msg = Message(messageSubject,recipients = recipients)
-	msg.body = messageBody
-	mail.send(msg)
-	return 'Mail Sent'
+# Sample usage: sendEmail("test-subject", "test-body", ["recipient1@gmail.com","recipient2@gmail.com",..])
+def sendEmail(messageSubject: str, messageBody: str, recipients: list):
+	with app.app_context():
+		for recipient in recipients:          
+			msg = Message(messageSubject, recipients = [recipient])
+			msg.body = messageBody
+			thr = Thread(target=send_email_thread, args=[msg])
+			thr.start()
+	return 'Mails Sent'

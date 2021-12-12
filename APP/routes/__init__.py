bf6b3a231af47bbcbe8041b7521ca00288b26b3a
Holmes7 <ashutoshpatkar@yahoo.in>
2021-12-12 18:59:57 +0530

    Sendotp route implemented

--- APP/routes/__init__.py
+++ APP/routes/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from .test.test import test_bp
+from .login import login_bp
 
 base_bp = Blueprint('base', __name__, url_prefix="/")
 
 base_bp.register_blueprint(test_bp)
+base_bp.register_blueprint(login_bp)

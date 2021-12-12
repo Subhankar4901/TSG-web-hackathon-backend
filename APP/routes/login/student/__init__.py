d530586c7fecf2767a393487b667873a736819b6
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-12 23:47:39 +0530

    added verify-otp route

--- APP/routes/login/student/__init__.py
+++ APP/routes/login/student/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from .sendotp import sendotp_bp
+from .verifyotp import verifyotp_bp
 
 student_bp = Blueprint('student', __name__, url_prefix="/student")
 
 student_bp.register_blueprint(sendotp_bp)
+student_bp.register_blueprint(verifyotp_bp)

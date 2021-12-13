e69f37d7ebabcdc48b306533d7672bd9e0dcae74
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-13 19:14:32 +0530

    minor bug

--- APP/routes/updateusers/__init__.py
+++ APP/routes/updateusers/__init__.py
@@ -3,5 +3,5 @@ from .students import students_bp
 from flask import Blueprint
 
 updateusers_bp=Blueprint('updateusers',__name__,url_prefix="/updateusers")
-updateusers_bp.register(officials_bp)
-updateusers_bp.register(students_bp)
+updateusers_bp.register_blueprint(officials_bp)
+updateusers_bp.register_blueprint(students_bp)

4e9626584399db5dd4032d32a8b6036f806b695b
Aryan Singh <ary1733@gmail.com>
2021-12-13 20:47:44 +0530

    loginOfficial

--- APP/routes/login/official/auth.py
+++ APP/routes/login/official/auth.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, make_response, jsonify
+from ....models import User
+from ....utils.jwt import JWT
 import csv
 # input
 # {
@@ -7,7 +9,10 @@ import csv
 # }
 # output
 # 
-# 
+# {
+#     "response": 1,
+#     "token": "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoyLCJ0eXBlIjpudWxsLCJ1c2VybmFtZSI6InRzZ19vZmZpY2lhbCJ9.2iA22zSCbGngzr7fpBfPjE2e0Kng2xHD7CxA88ejy340ol5z3HyUSGJ8Nlsq5pLJ6mOhhHkpNSOB87blIiyxdg"
+# }
 
 auth_bp = Blueprint('auth', __name__, url_prefix="/auth")
 
@@ -17,14 +22,23 @@ def auth():
 	data = request.get_json()
 	user = data["username"]
 	password=data["password"]
-
-	reader = csv.reader(open("docs/official.csv", 'r'))
-	for row in reader:
-		if row[0] == user and row[1] == password:
-			return "User found " + row[3]
-		elif row[0] == user:
-			return "Password incorrect"
-	return "NA"
+	user_obj=User.query.filter_by(username=user).first()
+	if user_obj:
+		if password == user_obj.password:
+			resp=make_response(jsonify(response=1,token=JWT.tokenizer({"user_id":user_obj.id,"type":user_obj.type, "username": user})))
+			resp.status_code=200
+			resp.headers.add("Content-Type","aplication/json")
+			return resp
+		else:
+			resp=make_response(jsonify(response=0))
+			resp.status_code=200
+			resp.headers.add("Content-Type","aplication/json")
+			return resp
+	else:
+		resp=make_response(jsonify(response=-1))
+		resp.status_code=200
+		resp.headers.add("Content-Type","aplication/json")
+		return resp
 		
 
 	

3d9052a30ae59e164e4ea0498a48adb9ed1e1634
Godzilla5111 <singhanurag5111@gmail.com>
2021-12-13 20:19:18 +0530

    Trying to access event model

--- APP/routes/home.py
+++ APP/routes/home.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from ..models.User import User
+from ..models.Event import Event
 base_bp = Blueprint('base', __name__, url_prefix="/")
 
 @base_bp.route('/')
 def home():
+    print(Event.query.limit(1).all()[0])
     return "home route"

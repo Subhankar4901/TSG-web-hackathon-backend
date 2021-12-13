fd56b1fc0eb30432092a4d6cbb3ef509d7fb62f9
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-13 19:48:16 +0530

    added token validation in update user api s

--- APP/routes/updateusers/officials.py
+++ APP/routes/updateusers/officials.py
@@ -1,9 +1,16 @@
-from flask import Blueprint,request,make_response,jsonify
+from flask import Blueprint, json,request,make_response,jsonify
 from ...utils.sheet import Sheet
+from ...utils.jwt import JWT
 officials_bp=Blueprint('officials',__name__,url_prefix="/officials")
 
 # login required of officials
 @officials_bp.route('/',methods=['POST'])
 def updateOfficials():
+	data=request.get_json()
+	userData = JWT.validator(data.token)
+	if not userData:
+		return make_response(jsonify({"message":"Unauthorized access."}), 401)
+	
+	# TODO allow only a particular type of user 
 	Sheet.get_officials()
 	return jsonify({"message": "officals updated in the database"})

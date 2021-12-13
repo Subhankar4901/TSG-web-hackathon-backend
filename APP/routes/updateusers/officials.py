78790377ac5b63ed19e50b1fba3b562ffead3a7f
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-13 19:15:33 +0530

    added comments

--- APP/routes/updateusers/officials.py
+++ APP/routes/updateusers/officials.py
@@ -2,6 +2,7 @@ from flask import Blueprint,request,make_response,jsonify
 from ...utils.sheet import Sheet
 officials_bp=Blueprint('officials',__name__,url_prefix="/officials")
 
+# login required of officials
 @officials_bp.route('/',methods=['POST'])
 def updateOfficials():
 	Sheet.get_officials()
--- APP/routes/updateusers/students.py
+++ APP/routes/updateusers/students.py
@@ -2,6 +2,7 @@ from flask import Blueprint,request,make_response,jsonify
 from ...utils.sheet import Sheet
 students_bp=Blueprint('students',__name__,url_prefix="/students")
 
+# login required of officials
 @students_bp.route('/',methods=['POST'])
 def updateStudents():
 	Sheet.get_students()

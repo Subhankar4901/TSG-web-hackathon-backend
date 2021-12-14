2044c73f66a3e324f1517d0bcadbd449b4934465
Godzilla5111 <singhanurag5111@gmail.com>
2021-12-15 01:47:42 +0530

    Created event/basic_details route
    Sending a get request to this route will fetch all the current and upcoming events with their basic details like -:
    *id,
    *title
    *introduction
    *poster
    *start-date
    *end-date
    *type
    *event_tags

--- APP/routes/__init__.py
+++ APP/routes/__init__.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from .login import login_bp
 from .updateusers import updateusers_bp
+from .event.events import events_bp
 
 base_bp = Blueprint('base', __name__, url_prefix="/")
 
 base_bp.register_blueprint(login_bp)
 base_bp.register_blueprint(updateusers_bp)
+base_bp.register_blueprint(events_bp)

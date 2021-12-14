efd95e876fe433fbb636d8df9e4647896b37c33a
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-14 16:53:01 +0530

    ignoring the first row in the sheet

--- APP/utils/sheet.py
+++ APP/utils/sheet.py
@@ -12,22 +12,34 @@ class Sheet:
     def get_students(cls):
         f=open(cls.student_sheet,newline="")
         student_reader=csv.reader(f)
+        r_n = 0
         for row in student_reader:
+            if not r_n:
+                r_n += 1
+                continue
+                
             user = User.query.filter_by(email=row[0]).first()
             if not user:
                 user = User(email=row[0], name=row[1], roll=row[2], type=4)
                 db.session.add(user)
                 db.session.commit()
+
         f.close()
     
     @classmethod
     def get_officials(cls):
         f=open(cls.official_sheet,newline="")
         official_reader=csv.reader(f)
+        r_n = 0
         for row in official_reader:
+            if not r_n:
+                r_n += 1
+                continue
+        
             user = User.query.filter_by(email=row[0]).first()
             if not user:
                 user = User(username=row[0], password=row[1], email=row[2], name=row[3], type=parse_official_type(row[0]))
                 db.session.add(user)
                 db.session.commit()
+
         f.close()

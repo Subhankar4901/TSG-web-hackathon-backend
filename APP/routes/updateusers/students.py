ecbd321ba7f67a275980c7e31513916ea240ed4f
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-14 16:11:16 +0530

    allowing only admin to update students

--- APP/routes/updateusers/students.py
+++ APP/routes/updateusers/students.py
@@ -1,16 +1,17 @@
 from flask import Blueprint,request,make_response,jsonify
 from ...utils.sheet import Sheet
 from ...utils.jwt import JWT
+from ...models import User
+
 students_bp=Blueprint('students',__name__,url_prefix="/students")
 
 # login required of officials
 @students_bp.route('/',methods=['POST'])
 def updateStudents():
 	data=request.get_json()
-	userData = JWT.validator(data.token)
-	if not userData:
+	userData = JWT.validator(data["token"])
+	if (not userData) or (User.query.filter_by(id=userData["id"]).first().type != 1):
 		return make_response(jsonify({"message":"Unauthorized access."}), 401)
-	
-	# TODO allow only a particular type of user :
+
 	Sheet.get_students()
 	return jsonify({"message": "students updated in the database"})

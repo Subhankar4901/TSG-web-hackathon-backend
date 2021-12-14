be3915b2671b29667749e3dd709bd645203e8698
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-14 12:44:39 +0530

    removed unnecessary files

--- APP/routes/test/test.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from ...utils.sendEmail import sendEmail
-test_bp = Blueprint('test', __name__, url_prefix="/test")
-
-@test_bp.route('/')
-def test():
-    return "hello"

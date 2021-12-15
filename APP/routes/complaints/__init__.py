0c58206eea647bed35dfabc0ed604101b8f13ecc
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-16 00:28:52 +0530

    added give_remark route

--- APP/routes/complaints/__init__.py
+++ APP/routes/complaints/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from .getcomplaints import getcomplaints_bp
+from .give_remark import remark_bp
 
 complaints_bp = Blueprint('complaints', __name__, url_prefix="/complaints")
 
 complaints_bp.register_blueprint(getcomplaints_bp)
+complaints_bp.register_blueprint(remark_bp)
 
---
+++ APP/routes/complaints/give_remark.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, json, request, make_response, jsonify
+from ...models import Complain
+from ...utils.jwt import JWT
+from ...models import Complain
+from ... import db
+
+remark_bp = Blueprint('remark', __name__, url_prefix="/addRemark")
+
+@remark_bp.route("/", methods=["POST"])
+def giveRemark():
+	data = request.get_json()
+	token = data.get("token")
+	complain_id = data.get("complain_id")
+	remarks = data.get("remark")
+	token = data.get("token")
+	token_dict = JWT.validator(token)
+	if token_dict and complain_id and remarks:
+		user_type = token_dict.get("type")
+		if user_type < 3:
+			complain_row = Complain.query.get(complain_id)
+			complain_row.remarks = remarks
+			db.session.add(complain_row)
+			db.session.commit()
+			return make_response(jsonify(message=f"remark updated as `{remarks}`"))
+	return make_response(jsonify(message="Invalid request"), 401)

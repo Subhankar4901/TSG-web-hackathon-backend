2b481ad36acca890ec1aee7ef6a0c855fbebb7f9
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-15 21:13:57 +0530

    little tweak for archives

--- APP/routes/events/ended_events.py
+++ APP/routes/events/ended_events.py
@@ -3,14 +3,22 @@ from flask import Blueprint,request,jsonify,make_response
 from ...models.Event import Event
 import datetime
 ended_events_bp=Blueprint("ended_events",__name__,url_prefix="/ended")
+
+# query parameters
+# t is time window for a event to be in results or in archives
+# setting archive loads archive and avoiding it will load current results
 @ended_events_bp.route("")
 @ended_events_bp.route("/")
 def ended_events():
     t=float(request.args.get("t")) # In days.
+    archive = request.args.get("archive") != None
     today=datetime.date.today()
     today_datetime=datetime.datetime(today.year,today.month,today.day)
     datetime_before_t_days=today_datetime-datetime.timedelta(days=t)
-    required_events=Event.query.filter(Event.end>=datetime_before_t_days).filter(Event.end<today_datetime).all()
+    if not archive:
+        required_events=Event.query.filter(Event.end>=datetime_before_t_days).filter(Event.end<today_datetime).all()
+    else:
+        required_events=Event.query.filter(Event.end<datetime_before_t_days).all()
     events=[]
     for event in required_events:
         events.append({

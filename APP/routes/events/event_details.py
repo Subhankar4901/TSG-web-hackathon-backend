6a67ea332e49f12f73da05d52bf495556bacabe0
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-23 17:15:07 +0530

    removed poster and report from event details api

--- APP/routes/events/event_details.py
+++ APP/routes/events/event_details.py
@@ -9,26 +9,19 @@ info_bp=Blueprint("info",__name__,url_prefix="/")
 # response is the list of achievements
 @info_bp.route("<event_id>/info/")
 @info_bp.route("<event_id>/info")
-def getAchievements(event_id):
-    token = request.args.get("token")
-    token_dict = JWT.validator(token)
-
-    if token_dict:
-        event = Event.query.get(event_id)
-        resp = make_response(jsonify(
-            title=event.title,
-            introduction=event.introduction,
-            procedure=event.procedure,
-            jugde_criteria=event.jugde_criteria,
-            timeline=event.timeline,
-            venue=event.venue,
-            start=event.start,
-            end=event.end,
-            organiser=event.organiser.name,
-            event_tags=event.event_tags,
-            type=event.type,
-            report=event.report.decode('utf-8'),
-            poster=event.poster.decode('utf-8')
-        ))
-        return resp
-    return make_response(jsonify(message="Unauthorized Access"), 401)
+def getInfo(event_id):
+    event = Event.query.get(event_id)
+    resp = make_response(jsonify(
+        title=event.title,
+        introduction=event.introduction,
+        procedure=event.procedure,
+        jugde_criteria=event.jugde_criteria,
+        timeline=event.timeline,
+        venue=event.venue,
+        start=event.start,
+        end=event.end,
+        organiser=event.organiser.name,
+        event_tags=event.event_tags,
+        type=event.type
+    ))
+    return resp

062916d890f03f061403b728e7e6054b6fdc188c
Aryan Singh <ary1733@gmail.com>
2021-12-24 13:35:14 +0530

    api updated for error messgages

--- APP/routes/login/student/verifyotp.py
+++ APP/routes/login/student/verifyotp.py
@@ -10,8 +10,15 @@ verifyotp_bp = Blueprint('verifyotp', __name__, url_prefix="/verifyotp")
 @verifyotp_bp.route("", methods=["POST"])
 def verifyotp():
 	data = request.get_json()
-	user_email = data["email"]
-	user_otp = int(data["otp"])
+	user_email = data.get("email")
+	user_otp = data.get("otp")
+	if user_otp.isnumeric():
+		user_otp = int(user_otp)
+	else:
+		resp=make_response(jsonify(message="otp invalid",email=None, password="OTP invalid"))
+		resp.headers.add("Content-Type","aplication/json")
+		resp.status_code=401
+		return resp
 	valid_time = 5 # in minutes
 	saved_data = OTP.query.filter_by(email=user_email).first()
 
@@ -25,24 +32,24 @@ def verifyotp():
 
 	if (saved_data.otp == user_otp):
 		if (timeDelta.total_seconds() > valid_time*60):
-			resp=make_response(jsonify(message="otp expired"))
+			resp=make_response(jsonify(message="otp expired",email=None, password="OTP expired"))
 			resp.headers.add("Content-Type","aplication/json")
 			resp.status_code=401
 		else:
 			user = User.query.filter_by(email=user_email).first()
 			if user and (int(user.type) == 4):
-				resp=make_response(jsonify(message="user authenticated",token=JWT.tokenizer({"id":user.id,"type":user.type}), user_type=user.type))
+				resp=make_response(jsonify(message="user authenticated",email=None, password=None, token=JWT.tokenizer({"id":user.id,"type":user.type}), user_type=user.type))
 				resp.headers.add("Content-Type","aplication/json")
 				resp.status_code=200
 			else:
-				resp=make_response(jsonify(message="generate a otp first"))
+				resp=make_response(jsonify(message="user not found", email="Wrong Email", password=None))
 				resp.headers.add("Content-Type","aplication/json")
 				resp.status_code=401
 
 		db.session.delete(saved_data)
 		db.session.commit()
 	else:
-		resp=make_response(jsonify(message="OTP invalid"))
+		resp=make_response(jsonify(message="OTP mismatch", email=None, password="Wrong OTP"))
 		resp.headers.add("Content-Type","aplication/json")
 		resp.status_code=401
 

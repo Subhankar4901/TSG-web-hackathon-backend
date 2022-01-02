619d12e78bef6dc0e840742142a4e14a09c21755
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2022-01-02 10:42:28 +0530

    small change

--- APP/routes/complaints/addcomplaints.py
+++ APP/routes/complaints/addcomplaints.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, make_response, jsonify
+from flask import Blueprint, json, request, make_response, jsonify
 from ...models import Complain
 from ...utils.jwt import JWT
 from ...utils import access_required
@@ -7,12 +7,13 @@ addcomplaint_bp = Blueprint('addcomplaint', __name__, url_prefix="/addcomplaint"
 
 @addcomplaint_bp.route("/", methods=["POST"])
 @addcomplaint_bp.route("", methods=["POST"])
-@access_required(4)
 def addcomplaint():
     data = request.form
     token = request.cookies.get("token")
     token_dict=JWT.validator(token)
-    mypdf = request.files.get("file")
+    if not token_dict:
+        return make_response(jsonify(message="Unauthorised"), 401)
+    mypdf = request.files.get("attachment")
     readPdf = mypdf.read()
     complain = Complain(
         userid = token_dict.get("id"),

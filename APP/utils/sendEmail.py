814f190c6e00a52f6d927921c0a66aa59c9340c1
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2022-01-15 22:25:41 +0530

    added some email notifications

--- APP/utils/sendEmail.py
+++ APP/utils/sendEmail.py
@@ -1,5 +1,6 @@
 from flask_mail import Message
 from .. import app, mail
+from ..models import User
 from threading import Thread
 
 # function which will be excuted in threads
@@ -9,7 +10,14 @@ def send_email_thread(msg):
 
 # the sender should allow less secure apps for this to work
 # Sample usage: sendEmail("test-subject", "test-body", ["recipient1@gmail.com","recipient2@gmail.com",..])
-def sendEmail(messageSubject: str, messageBody: str, recipients: list):
+def sendEmail(messageSubject: str, messageBody: str, recipients: list, all=False, officials=False):
+	if all:
+		users = User.query.all()
+		recipients = [user.email for user in users]
+	if officials:
+		users = User.query.filter(User.type<3).all()
+		recipients = [user.email for user in users]
+
 	with app.app_context():
 		for recipient in recipients:          
 			msg = Message(messageSubject, recipients = [recipient])
